88ed8755215a425afbe511051c6f0eec0b57e3ec
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-02-26 00:07:13 +0530

    hovering image successful

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,31 @@
 const src = "https://i.ibb.co/7RmbXTs/t-Spongebob-Squarepants-Band-Geeks.jpg";
 const canvas = document.getElementsByTagName("canvas");
-const ctx = canvas.getContext("2d");
-if (ctx) {
-    let img = new Image();
-    const imgPos = { x: 50, y: 0 };
-    let { x, y } = imgPos;
-    img.addEventListener("load", () => {
-        ctx.drawImage(img, x, y, 300, 300);
-    }, false);
-    img.src = src;
+const item = canvas.item(0);
+if (item) {
+    const ctx = item.getContext("2d");
+    if (ctx) {
+        let img = new Image();
+        const imgData = { x: 50, y: 50, width: 300, height: 300 };
+        let { x, y, width, height } = imgData;
+        let lineWidth = 3;
+        img.addEventListener("load", () => {
+            ctx.drawImage(img, x, y, width, height);
+        }, false);
+        img.src = src;
+        item.addEventListener("mousemove", (e) => {
+            if (e.clientX <= width + x &&
+                e.clientX >= x &&
+                e.clientY <= height + y &&
+                e.clientY >= y) {
+                ctx.strokeStyle = "orange";
+                ctx.lineWidth = lineWidth;
+                ctx.strokeRect(x, y, width, height);
+            }
+            else {
+                ctx.clearRect(x - 3, y - 3, width + 6, height + 6);
+                ctx.drawImage(img, x, y, width, height);
+            }
+        });
+    }
 }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,51 @@
 const src = "https://i.ibb.co/7RmbXTs/t-Spongebob-Squarepants-Band-Geeks.jpg";
 
-// @ts-ignore
-const canvas = document.getElementsByTagName("canvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
-if (ctx) {
-  let img = new Image();
-  const imgPos = { x: 50, y: 0 };
-  let { x, y } = imgPos;
+interface ImgI {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
 
-  img.addEventListener(
-    "load",
-    () => {
-      ctx.drawImage(img, x, y, 300, 300);
-    },
-    false
-  );
-  img.src = src;
+const canvas: HTMLCollectionOf<HTMLCanvasElement> = document.getElementsByTagName(
+  "canvas"
+);
+
+const item = canvas.item(0);
+
+if (item) {
+  const ctx = item.getContext("2d");
+  if (ctx) {
+    let img = new Image();
+    const imgData: ImgI = { x: 50, y: 50, width: 300, height: 300 };
+    let { x, y, width, height } = imgData;
+    let lineWidth = 3;
+    // let isFocussed = false;
+
+    img.addEventListener(
+      "load",
+      () => {
+        ctx.drawImage(img, x, y, width, height);
+      },
+      false
+    );
+    img.src = src;
+
+    // Events
+    item.addEventListener("mousemove", (e: MouseEvent) => {
+      if (
+        e.clientX <= width + x &&
+        e.clientX >= x &&
+        e.clientY <= height + y &&
+        e.clientY >= y
+      ) {
+        ctx.strokeStyle = "orange";
+        ctx.lineWidth = lineWidth;
+        ctx.strokeRect(x, y, width, height);
+      } else {
+        ctx.clearRect(x - 3, y - 3, width + 6, height + 6);
+        ctx.drawImage(img, x, y, width, height);
+      }
+    });
+  }
 }
